047b0276f229bdb9eebc2e2dcd5826a939d7d499
Story <astory11@yahoo.com>
2017-06-16 10:03:41 -0500

    removed main

--- src/main.rs
+++
@@ -1,7 +0,0 @@
-extern crate yarns;
-
-fn main() {
-    let val1 = String::from("hello");
-    let val2 = String::from("goodbye");
-    println!("The concat of 'hello' 'goodbye' is {}", yarns::concat(&val1, &val2));
-}
