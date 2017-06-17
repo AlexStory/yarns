8e93e4429433e033bab04ac34c74253f291943cd
Alex Story <astory11@yahoo.com>
2017-06-17 10:51:05 -0500

    added contains fn and documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,50 @@
+/// Gets the first char of a string, and returns it as string.
+///
+/// ```
+/// let hello = String::from("Hello");
+/// let first = yarns::head(hello);
+/// //first = (String)"H"
+/// ```
+
 pub fn head(word: &String) -> String {
     word.chars().take(1).collect()
 }
+
+/// Returns a new string starting at the index provided in the second argument, going the length provided as the third argument.
+///
+/// ```
+/// let hello = String::from("Hello");
+/// let sub= yarns::substr(&hello, 1, 3);
+/// //sub = (String) "ell"
+/// ```
 
 pub fn substr(word: &String, start:usize, length:usize) -> String {
     let res: String = word.chars().skip(start).take(length).collect();
     res
 }
 
+/// Concatenates two strings.
+///
+/// ```
+/// let hello = String::from("Hello");
+/// let world = String::from("World");
+/// let hw = yarns.concat(&hello, &world);
+/// //hw = (String)"HelloWorld"
+
 pub fn concat(str1: &String, str2: &String) -> String {
     format!("{}{}", str1, str2)
+}
+
+/// Returns wether a given String contains the given Substring.
+///
+/// ```
+/// let hello = String::from("Hello");
+/// let ell = String::from("ell");
+/// let has_it = yarns.contains(&hello, &ell);
+/// //has_it = (bool) true
+
+pub fn contains(main: &String, sub: &String) -> bool {
+    main[..].contains(&sub[..])
 }
 
 #[cfg(test)]
@@ -29,5 +65,12 @@ mod tests {
         let val1 = String::from("Hello");
         let val2 = String::from("Goodbye");
         assert_eq!("HelloGoodbye", ::concat(&val1, &val2));
+    }
+
+    #[test]
+    fn contains_hello_hell() {
+        let val1 = String::from("hello");
+        let val2 = String::from("hell");
+        assert_eq!(true, ::contains(&val1, &val2));
     }
 }
